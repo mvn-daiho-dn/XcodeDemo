92e0dfa4555765c32526883a38e6ada1b907efc7
Dai Ho V <dai.ho@monstar-lab.com>
2022-08-11 15:11:18 +0700

    Init project

---
+++ XcodeCloudDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeCloudDemo
+//
+//  Created by Dai Ho V. VN.Danang on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
